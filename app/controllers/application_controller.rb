fe6a64f77af0bbac5584cad12fd31462ecae4ddd
hlinforest <hllin@sfu.ca>
2015-07-22 00:59:08 +0000

    v1

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  protected
+
+  def restrict_access
+    if !current_user
+      flash[:alert] = "You must log in."
+      redirect_to new_session_path
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||  @current_user = User.find(session[:user_id])
+      #if @ is nil or false, set it to = User, otherwise do nothing
+    end
+    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to movies_path, notice: "Welcome home, #{user.firstname}!"
+    else
+      flash.now[:alert] = "log in failed..."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "RIP"
+  end
+end
